c7e4bf0334d8cbfcf03ae749be3a99a45ff2a21c
Pete Hanner <petehanner@gmail.com>
2020-12-31 10:13:34 -0500

    Overwrite home page

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return(
+    <div>
+      <h1>College Football Performance Ratio Stats</h1>
+      <h2>By Pete Hanner</h2>
+    </div>
+    )
+  }
+
+  export default Header;
