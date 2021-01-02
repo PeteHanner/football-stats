baecf49cbfd640805df7a1c8a7b66681a4fecead
Pete Hanner <petehanner@gmail.com>
2021-01-02 17:36:33 -0500

    Format header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react"
+import { Heading, Center } from "@chakra-ui/react"
 
 const Header = () => {
   return(
-    <>
+    <Center textAlign="center" m="2rem">
       <Heading as='h1'>College Football Performance Ratio Stats</Heading>
-    </>
+    </Center>
     )
   }
 
