9cc462b320d71ae0f3efbde9e138221efb71233b
Pete Hanner <petehanner@gmail.com>
2021-05-06 20:18:29 -0400

    Add router to app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import StatsTable from './components/StatsTable/StatsTable';
 
 function App() {
   return (
-    <>
-      <Header/>
-      <StatsTable />
-    </>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Header/>
+          <StatsTable />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
