736cb6cd49c990f52d85cd2fffd244812f73a455
Pete Hanner <petehanner@gmail.com>
2021-05-06 21:37:22 -0400

    Start to set up fetch

--- src/components/TeamPage/TeamPage.jsx
+++ src/components/TeamPage/TeamPage.jsx
@@ -1,11 +1,37 @@
-import React from "react"
-import { useParams } from "react-router-dom"
+import React, { useContext, useCallback, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from '../../store';
+import { Center } from '@chakra-ui/react';
+import axios from 'axios';
 
 const TeamPage = () => {
+  const [state, dispatch] = useContext(Context)
   const { name } = useParams()
+
+  const fetchTeam = useCallback((name) => {
+    let fetchUrl;
+    if (process.env.NODE_ENV === 'production') {
+      fetchUrl = `https://pete-hanner-football-stats-api.herokuapp.com/teams/${name}`
+    } else {
+      fetchUrl = `http://localhost:3001/teams/${name}`
+    }
+
+    // dispatch({ type: "FETCH_TEAM_START" })
+    axios.get(fetchUrl)
+    .then(response => {
+      debugger
+    })
+  }, [dispatch])
+
+  useEffect(() => {
+    fetchTeam(name)
+  }, [fetchTeam, name])
+
   return(
-    `Team Page for ${name}!`
+    <Center>
+      Team Page for {name}!
+    </Center>
   )
 }
 
-export default TeamPage
+export default TeamPage;
